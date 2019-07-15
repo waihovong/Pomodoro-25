50b18c6929aa1631511cf4579f6fdfc6f04ac28e
Wai Ho <waihovong@gmail.com>
2019-07-15 18:02:51 +0930

    deleted test files

--- test/startButtonTest.js
+++
@@ -1,8 +0,0 @@
-const assert = reuqire('chai').assert;
-const startButton = require('../src').startButton;
-
-describe('Start', function(){
-    it('start button should be pressed', function(){
-        assert.equal(app(), 'start');
-    })
-})
